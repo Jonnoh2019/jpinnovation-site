b1f7058e7518a50693322a8fba7e1d159afc6ec7
Jonnoh2019 <59282520+Jonnoh2019@users.noreply.github.com>
2026-07-19 22:28:59 +0100

    Bump service worker for profile menu g fix

--- jp-service-worker.js
+++ jp-service-worker.js
@@ -1,4 +1,4 @@
-const JP_SW_VERSION = "jp-sw-20260719-profile-menu-avatar-f";
+const JP_SW_VERSION = "jp-sw-20260719-profile-menu-avatar-g";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
