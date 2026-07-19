593cea1f378b2b75cc87be50c380a02c9623625c
Jonnoh2019 <59282520+Jonnoh2019@users.noreply.github.com>
2026-07-19 23:43:31 +0100

    Bump service worker for stable admin route

--- jp-service-worker.js
+++ jp-service-worker.js
@@ -1,4 +1,4 @@
-const JP_SW_VERSION = "jp-sw-20260719-profile-menu-critical-nav-b";
+const JP_SW_VERSION = "jp-sw-20260719-admin-route-stability-a";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
