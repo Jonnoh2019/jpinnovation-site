ce15d1401470690b0b40ae25ceeca77af7f16e0c
Jonnoh2019 <59282520+Jonnoh2019@users.noreply.github.com>
2026-07-19 23:20:06 +0100

    Bump service worker for profile menu recovery

--- jp-service-worker.js
+++ jp-service-worker.js
@@ -1,4 +1,4 @@
-const JP_SW_VERSION = "jp-sw-20260719-profile-menu-critical-nav-a";
+const JP_SW_VERSION = "jp-sw-20260719-profile-menu-critical-nav-b";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
