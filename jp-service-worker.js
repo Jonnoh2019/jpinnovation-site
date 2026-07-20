86eaf5203d6f455c9359efc42764035dbce91e05
Jonnoh2019 <59282520+Jonnoh2019@users.noreply.github.com>
2026-07-20 08:16:23 +0100

    Bump profile menu PWA cache version

--- jp-service-worker.js
+++ jp-service-worker.js
@@ -1,4 +1,4 @@
-const JP_SW_VERSION = "jp-sw-20260720-profile-menu-final-c";
+const JP_SW_VERSION = "jp-sw-20260720-profile-menu-final-d";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
