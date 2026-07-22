73c4a2269c4a244967740641fd1b83cf6ebfa6f7
Jonnoh2019 <59282520+Jonnoh2019@users.noreply.github.com>
2026-07-22 20:37:07 +0100

    Bump cache for accounts and profile redesign

--- jp-service-worker.js
+++ jp-service-worker.js
@@ -1,4 +1,4 @@
-const JP_SW_VERSION = "jp-sw-20260722-menu-fullscreen-avatar-ring-2";
+const JP_SW_VERSION = "jp-sw-20260722-accounts-profile-mobile-redesign-1";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
