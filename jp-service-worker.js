9b7baf49bbb0ec7898e14244552e646650141160
Jonnoh2019 <59282520+Jonnoh2019@users.noreply.github.com>
2026-07-25 09:36:03 +0100

    Bump service worker for approval freeze fix

--- jp-service-worker.js
+++ jp-service-worker.js
@@ -1,4 +1,4 @@
-const JP_SW_VERSION = "jp-sw-20260722-compact-menu-popouts-1";
+const JP_SW_VERSION = "jp-sw-20260725-approval-freeze-fix-1";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
