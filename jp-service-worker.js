8dfd506159d901863a19ec920075781760ef437f
Jonnoh2019 <59282520+Jonnoh2019@users.noreply.github.com>
2026-07-27 22:35:26 +0100

    Bump service worker after menu freeze fix

--- jp-service-worker.js
+++ jp-service-worker.js
@@ -1,4 +1,4 @@
-const JP_SW_VERSION = "jp-sw-20260727-profile-menu-single-owner";
+const JP_SW_VERSION = "jp-sw-20260727-menu-freeze-owner-clean";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
